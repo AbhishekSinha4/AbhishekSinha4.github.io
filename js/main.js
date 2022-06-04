75ed6cd9b17de68fb7f26c3ac1fc7740d49d547d
Abhishek Sinha <abhisheksinha@Abhisheks-MacBook-Pro.local>
2022-06-03 23:00:35 -0400

    Basic Experience page set-up

--- js/main.js
+++ js/main.js
@@ -7,6 +7,29 @@ for(var i=0;i<tabs.length;i++){
 		for(var i=0;i<pages.length;i++){
 			pages[i].classList.remove("active");
 		}
+		for(var i=0;i<tabs.length;i++){
+			tabs[i].classList.remove("active");
+		}
 		document.querySelector("#page-"+this.id.split("-")[1]).classList.add("active")
+		this.classList.add("active")
 	})
+}
+
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.querySelector("nav");
+
+// Get the offset position of the navbar
+var sticky = navbar.getBoundingClientRect().top;
+console.log(sticky)
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+	console.log(sticky+"\n"+ window.pageYOffset)
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
